bc151c9e5ef300322ca76098b2de2213d6a7be67
David Alexandre <143817175+DavidASN@users.noreply.github.com>
2023-11-17 10:08:31 -0300

    Update Matriz2.java
    Posição adicionada

--- Matriz/Matriz2.java
+++ Matriz/Matriz2.java
@@ -1,12 +1,14 @@
-package javaProjectTPAMatriz;
 import java.util.Random;
-public class Matriz2 {
+public class Ex002 {
 	
 	public static void main(String[] args) {
 		Random ran = new Random();
 		int b [][] = new int[3][3];
 		
 		int menor = 100, maior = -100;
+		int posicaoXMA = 0,posicaoYMA = 0;
+		int posicaoXME = 0,posicaoYME = 0;
+		
 		
 		for(int i = 0; i< 3; i++) {
 			for(int j = 0 ; j< 3; j++) {
@@ -26,15 +28,20 @@ public class Matriz2 {
 		for(int i=0; i<3 ; i++) {
 			for(int j = 0; j<3 ; j++) {
 				if(b[i][j] < menor) {
-					menor = b[i][j]; 
+					menor = b[i][j];
+					posicaoXME = i+1;
+					posicaoYME = j+1;
 				}
 				if(b[i][j] > maior) {
-					maior = b[i][j]; 
+					maior = b[i][j];
+					posicaoXMA = i+1;
+					posicaoYMA = j+1;
 				}
 			}
 		}
-		System.out.println("Maior:"+maior);
-		System.out.println("Menor:"+menor);
+		System.out.println("Maior:"+maior+" esta na linha "+posicaoXMA+" e na coluna "+posicaoYMA);
+		System.out.println("Menor:"+menor+" esta na linha "+posicaoXME+" e na coluna "+posicaoYME);
+		
 	}
 
 }
